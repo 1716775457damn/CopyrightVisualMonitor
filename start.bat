28c22be8da9688d91297c236eb8d128ef5863b1e
thewang <1716775457@qq.com>
2026-04-07 14:38:33 +0800

    2.0.1

--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Copyright Visual Monitor...
-call conda activate jlc
-python main.py
-pause
--- 打包.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 开始打包软著自检助手...
-pyinstaller --noconfirm --onedir --windowed --add-data "templates;templates/" --add-data "chi_sim.traineddata;tessdata/" --hidden-import=win10toast main.py
-echo 打包完成！
-pause
